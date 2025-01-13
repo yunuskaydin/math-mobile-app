d97380a0551131e17bf7ac768bd80729b4bae331
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2025-01-13 19:12:55 +0300

    feat: add registration and login serializers for user authentication

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -2,6 +2,10 @@
 
 from rest_framework import serializers
 from .models import Folder, Video
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,3 +23,29 @@ class FolderSerializer(serializers.ModelSerializer):
     def get_subfolders(self, obj):
         subfolders = obj.subfolders.all()
         return FolderSerializer(subfolders, many=True).data
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
