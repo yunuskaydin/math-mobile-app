6f77d4c5c26cc1f5d4ddc34efb1d6fe76fd1f116
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2025-01-13 19:13:00 +0300

    feat: add registration and login views with authentication logic and permissions

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,13 +1,37 @@
 # backend/core/views.py
 
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, status
 from .models import Folder, Video
-from .serializers import FolderSerializer, VideoSerializer
+from .serializers import FolderSerializer, VideoSerializer, RegisterSerializer, LoginSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsAdminUser
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 class FolderViewSet(viewsets.ModelViewSet):
     queryset = Folder.objects.all()
     serializer_class = FolderSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminUser]
+
 
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminUser]
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
