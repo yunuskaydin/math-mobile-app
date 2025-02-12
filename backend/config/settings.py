147c170f0d58acd7ab66f9c2185470ce2f97ea52
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2025-02-12 10:07:49 +0300

    feat: update permission settings to allow public access for API endpoints

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,7 +49,8 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
