f19e442964b83960dc37c7e42659237d293dfd86
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2025-05-13 18:49:03 +0300

    feat: configure static media URL handling in URL patterns

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -44,4 +46,4 @@ urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
