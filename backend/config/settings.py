554aefa07a1735b676b70d1f4436706d537668ac
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2025-05-16 16:00:02 +0300

    feat: configure database connection settings and add database configuration for math_mobile

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #   'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': dj_database_url.config(
+        default='postgres://localhost/math_mobile',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
