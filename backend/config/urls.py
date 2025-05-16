42bcc95a2552604276e67f766d9d7a1918e11f8a
Yunus Kağan Aydın <yunus.aydin1@std.bogazici.edu.tr>
2025-05-16 15:55:29 +0300

    feat: add redirect to Swagger UI in URL configuration for improved access

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,6 +22,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from core.views import RegisterView, LoginView
+from django.views.generic import RedirectView
 
 
 schema_view = get_schema_view(
@@ -39,6 +40,7 @@ schema_view = get_schema_view(
     
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
